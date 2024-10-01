dc9882f4441220ef492f78e02d4822bf60289080
Miltov Danil <Dmiltov@bk.ru>
2024-10-01 12:54:03 +0700

    Upd js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Пример взаимодействия с пользователем: alert, prompt, confirm
-
 // Пример alert
 alert("Это кто зашел на ужин?");
 
@@ -13,26 +11,22 @@ if (isSure) {
     alert("Вы выбрали 'Да'.");
 } else {
     alert("Вы выбрали 'Нет'.");
-    // Добавление изображения на страницу при ответе "Нет"
     let imageContainer = document.getElementById("image-container");
     let img = document.createElement("img");
     img.src = "https://mrkishka.github.io/mrk.github.io/xzzx.jpg"; // Путь к вашему изображению
     img.alt = "Изображение для ответа 'Нет'";
-    img.style.width = "300px"; // Можно задать размер изображения
+    img.style.width = "3000px";
     imageContainer.appendChild(img);
 }
-
-// Добавляем таблицу для второго пункта (циклы и switch)
 document.body.innerHTML += "<table><tr><th>Циклы и switch</th></tr>";
 
-// Пример цикла for
+
 let content = "<tr><td><h2>Цикл for</h2>";
 for (let i = 1; i <= 5; i++) {
     content += "Число: " + i + "<br>";
 }
 content += "</td></tr>";
 
-// Пример цикла while
 content += "<tr><td><h2>Цикл while</h2>";
 let count = 1;
 while (count <= 3) {
@@ -40,49 +34,43 @@ while (count <= 3) {
     count++;
 }
 content += "</td></tr>";
-
-// Пример оператора switch
-let day = new Date().getDay(); // Получаем текущий день недели (0 = Воскресенье, 1 = Понедельник и т.д.)
+let day = new Date().getDay();
 content += "<tr><td><h2>Оператор switch</h2>";
 switch (day) {
     case 0:
-        content += "Сегодня Воскресенье.<br>";
+        content += "Сегодня на топ.<br>";
         break;
     case 1:
-        content += "Сегодня Понедельник.<br>";
+        content += "Сегодня на мид.<br>";
         break;
     case 2:
-        content += "Сегодня Вторник.<br>";
+        content += "Сегодня на бот.<br>";
         break;
     case 3:
-        content += "Сегодня Среда.<br>";
+        content += "Сегодня в лес.<br>";
         break;
     case 4:
-        content += "Сегодня Четверг.<br>";
+        content += "Сегодня на Рошана.<br>";
         break;
     case 5:
-        content += "Сегодня Пятница.<br>";
+        content += "Сегодня под фонтан.<br>";
         break;
     case 6:
-        content += "Сегодня Суббота.<br>";
+        content += "Сегодня в трон.<br>";
         break;
     default:
-        content += "Неправильный день.<br>";
+        content += "Пуджа забанили.<br>";
 }
 content += "</td></tr>";
 document.body.innerHTML += content + "</table>";
 
-// Пример функции Function Expression
 
-// Function Expression (Функциональное выражение)
 const greet = function (name) {
-    return "Привет, " + name + "!";
+    return "О, как мило, " + name + "!";
 };
 
-// Добавляем таблицу для третьего пункта (Function Expression)
 document.body.innerHTML += "<table><tr><th>Function Expression</th></tr>";
 
-// Использование функции
 let greetingMessage = greet(userName);
 document.body.innerHTML += "<tr><td><h2>Пример Function Expression</h2>";
 document.body.innerHTML += "<h1>" + greetingMessage + "</h1>";
