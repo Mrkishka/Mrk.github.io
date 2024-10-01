54902caa4879c183b51e5e8b24e7ab6da72b6f12
Voronov Vadim <is341s06@csc.sibsutis.ru>
2024-10-01 14:15:17 +0700

    Fix js

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 // Пример alert
-alert("Это кто зашел на ужин?");
+alert('Это кто зашел на ужин?')
 
 // Пример prompt
-let userName = prompt("Познакомимся поближе.", "Meele creep");
-alert("Ах ты мелкий... " + userName + "!");
+let userName = prompt('Познакомимся поближе.', 'Meele creep')
+alert('Ах ты мелкий... ' + userName + '!')
 
 // Пример confirm
-let isSure = confirm("Фарш не провернуть назад, и мясо из котлет не восстановишь.");
+let isSure = confirm(
+	'Фарш не провернуть назад, и мясо из котлет не восстановишь.'
+)
 if (isSure) {
-    alert("Вы выбрали 'Да'.");
+	alert("Вы выбрали 'Да'.")
 } else {
-    alert("Вы выбрали 'Нет'.");
-    let imageContainer = document.getElementById("image-container");
-    let img = document.createElement("img");
-    img.src = "https://mrkishka.github.io/mrk.github.io/xzzx.jpg"; // Путь к вашему изображению
-    img.alt = "Изображение для ответа 'Нет'";
-    img.style.width = "500px";
-    imageContainer.appendChild(img);
+	alert("Вы выбрали 'Нет'.")
+	let imageContainer = document.getElementById('image-container')
+	let img = document.createElement('img')
+	img.src = 'https://mrkishka.github.io/mrk.github.io/xzzx.jpg' // Путь к вашему изображению
+	img.alt = "Изображение для ответа 'Нет'"
+	img.style.width = '500px'
+	imageContainer.appendChild(img)
 }
-document.body.innerHTML += "<table><tr><th>Циклы и switch</th></tr>";
+document.body.innerHTML += '<table><tr><th>Циклы и switch</th></tr>'
 
-
-let content = "<tr><td><h2>Цикл for</h2>";
+let content = '<table><tr><th>Цикл for</th></tr>'
 for (let i = 1; i <= 5; i++) {
-    content += "Число: " + i + "<br>";
+	content += 'Число: ' + i + '<br>'
 }
-content += "</td></tr>";
+content += '</td></tr>'
 
-content += "<tr><td><h2>Цикл while</h2>";
-let count = 1;
+content += '<table><tr><th>Цикл while</th></tr>'
+let count = 1
 while (count <= 3) {
-    content += "Цикл while, шаг " + count + "<br>";
-    count++;
+	content += 'Цикл while, шаг ' + count + '<br>'
+	count++
 }
-content += "</td></tr>";
-let day = new Date().getDay();
-content += "<tr><td><h2>Оператор switch</h2>";
+content += '</td></tr>'
+let day = new Date().getDay()
+content += '<table><tr><th>Оператор switch</th></tr>'
 switch (day) {
-    case 0:
-        content += "Сегодня на топ.<br>";
-        break;
-    case 1:
-        content += "Сегодня на мид.<br>";
-        break;
-    case 2:
-        content += "Сегодня на бот.<br>";
-        break;
-    case 3:
-        content += "Сегодня в лес.<br>";
-        break;
-    case 4:
-        content += "Сегодня на Рошана.<br>";
-        break;
-    case 5:
-        content += "Сегодня под фонтан.<br>";
-        break;
-    case 6:
-        content += "Сегодня в трон.<br>";
-        break;
-    default:
-        content += "Пуджа забанили.<br>";
+	case 0:
+		content += 'Сегодня на топ.<br>'
+		break
+	case 1:
+		content += 'Сегодня на мид.<br>'
+		break
+	case 2:
+		content += 'Сегодня на бот.<br>'
+		break
+	case 3:
+		content += 'Сегодня в лес.<br>'
+		break
+	case 4:
+		content += 'Сегодня на Рошана.<br>'
+		break
+	case 5:
+		content += 'Сегодня под фонтан.<br>'
+		break
+	case 6:
+		content += 'Сегодня в трон.<br>'
+		break
+	default:
+		content += 'Пуджа забанили.<br>'
 }
-content += "</td></tr>";
-document.body.innerHTML += content + "</table>";
-
+content += '</td></tr>'
+document.body.innerHTML += content + '</table>'
 
 const greet = function (name) {
-    return "О, как мило, " + name + "!";
-};
+	return 'О, как мило, ' + name + '!'
+}
 
-document.body.innerHTML += "<table><tr><th>Function Expression</th></tr>";
+document.body.innerHTML += '<table><tr><th>Function Expression</th></tr>'
 
-let greetingMessage = greet(userName);
-document.body.innerHTML += "<tr><td><h2>" + greetingMessage + "</h2>";
-document.body.innerHTML += "</td></tr></table>";
+let greetingMessage = greet(userName)
+document.body.innerHTML += '<table><tr><th>' + greetingMessage + '</th></tr>'
+document.body.innerHTML += '</td></tr></table>'
